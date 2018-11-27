2b9a5ae1b86efcc117ea29431b914be7c6a9498f
David Chambers <dlchambersjr@gmail.com>
2018-11-26 17:44:22 -0800

    more scaffolding for components and SASS

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>Some awesome cards and decks in here ...</div>
+    );
+  }
+}
+
+export default App;
