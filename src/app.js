81304975057408045aa149612eb6aecbcabd00d6
David Chambers <dlchambersjr@gmail.com>
2018-12-03 17:42:00 -0800

    list component finished

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-import './stylesheets/design.scss';
+// import './stylesheets/design.scss';
 
-import Header from './components/header/header.js';
-import Page from './components/page/page.js';
+// import Header from './components/header/header.js';
+// import Page from './components/page/page.js';
+import List from './components/list/list.js';
 import content from './data/content.json';
 
 class App extends React.Component {
@@ -11,20 +12,38 @@ class App extends React.Component {
     super(props);
     this.state = {
       content: content,
+      list: {
+        type: 'unordered', // unordered or ordered
+        items: ['mangos', 'dates', 'cherries'],
+      },
     };
   }
 
   render() {
     return (
       <React.Fragment>
-        <Header>
+        {/* FIXME:  The code below is temporarily commented out to highlight the list assignment. */}
+        {/* <Header>
           <div >
             <h1>
               HEADER TITLE HERE
             </h1>
           </div>
-        </Header>
-        <Page content={this.state.content} />
+        </Header> */}
+        <List
+          type={this.state.list.type}
+          items={this.state.list.items}
+        >
+          <ul>
+            <li>This is a test of props.children</li>
+          </ul>
+        </List>
+        <List>
+          <ul>
+            <li>This is a test of props.children</li>
+          </ul>
+        </List>
+        {/* <Page content={this.state.content} /> */}
       </React.Fragment>
 
     );
